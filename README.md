b7e5d05148108724fa266cbeb73c71888cc95218
Vini <viniciusicmsc@gmail.com>
2024-11-27 17:40:23 -0300

    portfólio

---
+++ README.md
@@ -0,0 +1,7 @@
+<div align="center">
+
+# 📂 Portfólio:
+
+#### Este repositório é direcionado à construção do Portfólio Técnico do Ensino Médio 
+
+</div>
---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+let currentSlide = 0;
+const slidesContainer = document.querySelector('.slides');
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function changeSlide() {
+    const offset = -(currentSlide * 100);
+    slidesContainer.style.transform = `translateX(${offset}%)`;
+    currentSlide = (currentSlide + 1) % totalSlides;
+}
+
+if (totalSlides > 0) {
+    setInterval(changeSlide, 8000);
+}
+
+function showArea(area) {
+    const areas = document.querySelectorAll('.subject');
+    areas.forEach(content => {
+        content.classList.remove('active');
+    });
+
+    document.getElementById(area).classList.add('active');
+}
